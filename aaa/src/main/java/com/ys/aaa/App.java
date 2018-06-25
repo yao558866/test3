032870d558525e5a5e8d6d4793978dbb5ae730c5
ystaishiji <53055975@qq.com>
2018-06-26 05:00:48 +0800

    a

---
+++ aaa/src/main/java/com/ys/aaa/App.java
@@ -0,0 +1,39 @@
+package com.ys.aaa;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test(dataProvider = "dp")
+	  public void f(Integer n, String s) {
+		  System.out.println("哈哈11:"+s);
+	  }
+	  @AfterMethod
+	  public void afterMethod() {
+	  }
+
+
+	  @DataProvider
+	  public Object[][] dp() {
+	    return new Object[][] {
+	      new Object[] { 1, "a" },
+	      new Object[] { 2, "b" },
+	    };
+	  }
+	  @BeforeClass
+	  public void beforeClass() {
+	  }
+
+	  @AfterClass
+	  public void afterClass() {
+	  }
+
+}
---
+++ aaa/src/test/java/com/ys/aaa/NewTest.java
@@ -0,0 +1,11 @@
+package com.ys.aaa;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class NewTest {
+  
+}
